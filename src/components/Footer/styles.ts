95235cf5a7913879621d47660192d3aec97e2f2c
Sergio Reza <sergiogrezach@gmail.com>
2026-02-27 20:52:34 -0600

    [FIX] Ajustes de Footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { FlexboxStructure, PxToRem } from "../../theme/styles";
 
 const FooterStructure = styled.footer`
